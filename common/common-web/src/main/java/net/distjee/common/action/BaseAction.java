2f780464b314d6419f5c29c0e2249100ca8da931
Jianxin <jianxin1009@163.com>
2016-01-21 11:32:11 +0800

    重构包结构，添加demo工程，并规划结构

---
+++ common/common-web/src/main/java/net/distjee/common/action/BaseAction.java
@@ -0,0 +1,12 @@
+package net.distjee.common.action;
+
+
+import net.distjee.common.model.BaseModel;
+import net.distjee.common.service.BaseService;
+
+/**
+ * Created by king on 2016/1/20 0020.
+ */
+public abstract class BaseAction <M extends BaseModel,S extends BaseService> {
+
+}
